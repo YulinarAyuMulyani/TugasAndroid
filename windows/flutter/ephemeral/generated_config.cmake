e1c138c6c02953ff82f15c1c4e08c7acc4b072fb
YulinarAyuMulyani <84649412+YulinarAyuMulyani@users.noreply.github.com>
2022-06-21 15:38:11 +0800

    android

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\flutter\\application_berita_b" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src"
+  "PROJECT_DIR=E:\\flutter\\application_berita_b"
+  "FLUTTER_ROOT=C:\\src"
+  "FLUTTER_EPHEMERAL_DIR=E:\\flutter\\application_berita_b\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\flutter\\application_berita_b"
+  "FLUTTER_TARGET=E:\\flutter\\application_berita_b\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\flutter\\application_berita_b\\.dart_tool\\package_config.json"
+)
